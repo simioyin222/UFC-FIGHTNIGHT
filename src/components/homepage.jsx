a5d525de109b218d2d74d257d9000b36ee39a237
simioyin222 <simi.oyinkolade@gmail.com>
2024-04-29 10:12:15 -0500

    import full calendar wdiget/applicatoin into overall app will begin full code interation into code now

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./FirstPageHomePage.css";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 export const FirstPageHomePage = () => {
   return (
