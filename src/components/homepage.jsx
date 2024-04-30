99a5e6ea26e899a2a5cf0d0168e6389bdee3fcec
simioyin222 <simi.oyinkolade@gmail.com>
2024-04-30 13:09:21 -0500

    some progress in terms of where elements for home page supposed to be the only issue is now images apperaing

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -28,7 +28,7 @@ const handleDateSelect = (selectInfo) => {
 
 export const HomePage = () => {
   return (
-    <div className="home-page">
+    <div className="first-page-home-page">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
@@ -49,44 +49,59 @@ export const HomePage = () => {
       />
       <div className="overlap-wrapper">
         <div className="overlap">
-        <img className="UFC-RING-PIC-first" alt="Ufc RING PIC first" src="../../assets/HOMEPAGE/UFC-RING-PIC-3-first-page-background.png" />
-        <img className="left-ufc-laughing" alt="Left ufc laughing" src="../assets/HOMEPAGE/left-ufc-laughing-guy-2.png" />
-        <img className="right-ufc-laughing" alt="Right ufc laughing" src="../assets/HOMEPAGE/right-ufc-laughing-guy-1.png" />
+          <img className="UFC-RING-PIC-first" alt="Ufc RING PIC first" src="UFC-RING-PIC-3-first-page-background.png" />
+          <img className="left-ufc-laughing" alt="Left ufc laughing" src="left-ufc-laughing-guy-2.png" />
+          <img className="right-ufc-laughing" alt="Right ufc laughing" src="right-ufc-laughing-guy-1.png" />
           <div className="FIGHT-text-FIRST">FIGHT</div>
           <div className="NIGHT-text-FIRST">NIGHT</div>
           <div className="whole-ufc-belt">
             <div className="overlap-group">
-            <img className="ufc-belt-FIRST-PAGE" alt="Ufc belt FIRST PAGE" src="../assets/HOMEPAGE/ufc-belt-FIRST-PAGE.png" />
+              <img className="ufc-belt-FIRST-PAGE" alt="Ufc belt FIRST PAGE" src="ufc-belt-FIRST-PAGE.png" />
               <div className="streaming-nav-bar">
                 <div className="div">
                   <div className="streaming-nav-bar-2" />
-                  <img className="streaming-icon-and" alt="Streaming icon and" src="../assets/HOMEPAGE/streaming-icon-and-text-first-page.png" />
+                  <img
+                    className="streaming-icon-and"
+                    alt="Streaming icon and"
+                    src="streaming-icon-and-text-first-page.png"
+                  />
                 </div>
               </div>
               <div className="catering-nav-bar">
                 <div className="overlap-2">
-                <div className="catering-nav-bar-box" />
-                <img className="UFC-RING-PIC-first" alt="Ufc RING PIC first" src="../../assets/HOMEPAGE/UFC-RING-PIC-3-first-page-background.png" />
-                <img className="catering-icon-and" alt="Catering icon and" src="../assets/HOMEPAGE/catering-icon-and-text-streaming-page.png" />
+                  <div className="catering-nav-bar-box" />
+                  <img
+                    className="catering-icon-and"
+                    alt="Catering icon and"
+                    src="catering-icon-and-text-streaming-page.png"
+                  />
                 </div>
               </div>
               <div className="opinions-nav-bar">
                 <div className="overlap-2">
                   <div className="opinions-nav-bar-box" />
-                  <img className="opinions-icon-and" alt="Opinions icon and" src="../assets/HOMEPAGE/opinions-icon-and-text-first-page.png" />
+                  <img
+                    className="opinions-icon-and"
+                    alt="Opinions icon and"
+                    src="opinions-icon-and-text-first-page.png"
+                  />
                 </div>
               </div>
               <div className="trending-nav-bar">
                 <div className="overlap-3">
-                <div className="opinions-nav-bar-box" />
-                <img className="trending-icon-and" alt="Trending icon and" src="../assets/HOMEPAGE/trending-icon-and-text-streaming-page.png" />
+                <div className="opinions-nav-bar-box" /> \\fix nav bar box issue 
+                  <img
+                    className="trending-icon-and"
+                    alt="Trending icon and"
+                    src="trending-icon-and-text-streaming-page.png"
+                  />
                 </div>
               </div>
             </div>
           </div>
           <div className="notfication-footer">
-          <img className="send-invite-send" alt="Send invite send" src="../assets/HOMEPAGE/send-reminder-icon.png" />
-          <img className="set-reminder-on" alt="Set reminder on" src="../assets/HOMEPAGE/set-reminder-on-device.png" />
+            <img className="send-invite-send" alt="Send invite send" src="send-reminder-icon.png" />
+            <img className="set-reminder-on" alt="Set reminder on" src="set-reminder-on-device.png" />
           </div>
         </div>
       </div>
