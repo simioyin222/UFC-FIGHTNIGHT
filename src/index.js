d553d18f66e429029c4a90c7ba3b8b6346f7e62b
simioyin222 <simi.oyinkolade@gmail.com>
2024-05-01 22:47:49 -0500

    adjust index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import emailjs from 'emailjs-com';
+
+// Initialize EmailJS with your user ID
+emailjs.init('your_user_id'); // Replace 'your_user_id' with your actual EmailJS user ID
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +19,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
